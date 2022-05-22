e2a40b5af93c847e34a7cb311f22450385ea3842
kubilay <kubilay.tsilkara@gmail.com>
2022-05-22 12:03:51 +0000

    gitignore

--- repo/cairo-abstract.py
+++ repo/cairo-abstract.py
@@ -1,4 +1,5 @@
 # musings with cairo by Kubilay Tsil Kara
+
 import cairo
 import math
 import random
@@ -23,30 +24,31 @@ ctx.set_source_rgb(0, 0, 0)
 ctx.fill()
 
 # drawing code with loops
-c1 = range(0,n,1)
+c1 = range(0, n, 1)
 c0 = list(itertools.chain.from_iterable(itertools.repeat(c1, n)))
-print('c0 is:',c0)
-d2 = np.array(range(0,n,1))
+print('c0 is:', c0)
+d2 = np.array(range(0, n, 1))
 d0 = np.repeat(d2, n)
-print('d0 is:',d0)
+print('d0 is:', d0)
 
-df = list(set(zip(d0,c0)))
-print('df is: ',df)
+df = list(set(zip(d0, c0)))
+print('df is: ', df)
 
-def circle(xc,yc):
+
+def circle(xc, yc):
     ctx.set_source_rgb(1, 1, 1)
-    ctx.arc(xc+0.5, yc+0.5,0.5, 0, 2 * math.pi)
-  
+    ctx.arc(xc+0.5, yc+0.5, 0.5, 0, 2 * math.pi)
 
-def box(xc,yc):
+
+def box(xc, yc):
     ctx.set_source_rgb(0, 0, 0)
     ctx.rectangle(xc, yc, 1, 1)
-  
 
-for xc,yc in df:
-    mylist = [circle,box]
+
+for xc, yc in df:
+    mylist = [circle, box]
     ctx.move_to(xc, xc)
-    np.random.choice(mylist,p=[0.4, 0.6])(xc,yc)
+    np.random.choice(mylist, p=[0.4, 0.6])(xc, yc)
     if circle in mylist:
         ctx.close_path()
         ctx.fill_preserve()
